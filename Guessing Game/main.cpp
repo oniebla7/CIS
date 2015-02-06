fe028b63ab92ac3ab9418b5f70af2b855e18b07a
Oscar Fuentes <oscarniebla@Oscars-MacBook-Pro.local>
2015-02-05 17:13:51 -0800

    Initial Commit

---
+++ Guessing Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Guessing Game
+//
+//  Created by Oscar Fuentes on 2/5/15.
+//  Copyright (c) 2015 Oscar Fuentes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
